6397af8bc1cda3bee062dc16597d15575da3cb1d
Newton Joshua <nejo@newtonjoshua.com>
2016-08-09 13:22:19 +0530

    Update solarcalc.js
    added npm support

--- solarcalc.js
+++ solarcalc.js
@@ -1,19 +1,33 @@
 // Equations based on NOAA’s Solar Calculator; all angles in radians.
 // http://www.esrl.noaa.gov/gmd/grad/solcalc/
 
-(function() {
+(function () {
 
-var test = function(){
-  return 'test';
-}
+  var test = function () {
+    return 'test';
+  }
+  
+  // The SolarCalc object that contains all the functions and objects
+  var SolarCalc = {
+    test: test
+  };
+  
+  // Expose SolarCalc as a global object
+  SolarCalc = SolarCalc;
 
-SolarCalc = {
-  test : test
-};
+  // Export the SolarCalc object for **Node.js**, with
+  // backwards-compatibility for their old module API. If we're in
+  // the browser, add `_` as a global object.
+  // (`nodeType` is checked to ensure that `module`
+  // and `exports` are not HTML elements.)
+  if (typeof exports != 'undefined' && !exports.nodeType) {
+    if (typeof module != 'undefined' && !module.nodeType && module.exports) {
+      exports = module.exports = SolarCalc;
+    }
+    exports.SolarCalc = SolarCalc;
+  } else {
+    root.SolarCalc = SolarCalc;
+  }
 
 })();
 
-
-exports.test = function() {
-  console.log("This is a message from the demo package");
-}
